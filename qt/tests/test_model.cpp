1515a062186319c190c9dbf16fb5b3128989229f
BoneMarrowGames <bonemarrowgames@gmail.com>
2026-05-04 12:52:09 +0300

    fix(ci): resolve clang-format failures - reformat test_model.cpp and limit check to Linux

--- qt/tests/test_model.cpp
+++ qt/tests/test_model.cpp
@@ -14,25 +14,91 @@ private slots:
     {
         CompareResultTableModel model;
         QVector<CompareRow> rows;
-        rows.push_back(CompareRow{QStringLiteral("A001/clip.mov"), QStringLiteral("Matching"), QStringLiteral("4 B"), QStringLiteral("4 B"), {}, {}, QString{}, QString{}, CompareRow::Matching, false});
-        rows.push_back(CompareRow{QStringLiteral("A001/audio.wav"), QStringLiteral("Changed"), QStringLiteral("8 B"), QStringLiteral("12 B"), {}, {}, QString{}, QString{}, CompareRow::Changed, false});
+        rows.push_back(CompareRow{QStringLiteral("A001/clip.mov"),
+                                  QStringLiteral("Matching"),
+                                  QStringLiteral("4 B"),
+                                  QStringLiteral("4 B"),
+                                  {},
+                                  {},
+                                  QString{},
+                                  QString{},
+                                  CompareRow::Matching,
+                                  false});
+        rows.push_back(CompareRow{QStringLiteral("A001/audio.wav"),
+                                  QStringLiteral("Changed"),
+                                  QStringLiteral("8 B"),
+                                  QStringLiteral("12 B"),
+                                  {},
+                                  {},
+                                  QString{},
+                                  QString{},
+                                  CompareRow::Changed,
+                                  false});
         model.setRows(rows);
 
         QCOMPARE(model.rowCount(), 2);
         QCOMPARE(model.columnCount(), 6);
-        QCOMPARE(model.data(model.index(0, 1), Qt::DisplayRole).toString(), QStringLiteral("A001/clip.mov"));
-        QCOMPARE(model.data(model.index(1, 0), CompareResultTableModel::StatusCodeRole).toInt(), CompareRow::Changed);
+        QCOMPARE(model.data(model.index(0, 1), Qt::DisplayRole).toString(),
+                 QStringLiteral("A001/clip.mov"));
+        QCOMPARE(
+            model.data(model.index(1, 0), CompareResultTableModel::StatusCodeRole).toInt(),
+            CompareRow::Changed);
     }
 
     void filtersByOperationalState()
     {
         CompareResultTableModel model;
         QVector<CompareRow> rows;
-        rows.push_back(CompareRow{QStringLiteral("match.mov"), QStringLiteral("Matching"), {}, {}, {}, {}, QString{}, QString{}, CompareRow::Matching, false});
-        rows.push_back(CompareRow{QStringLiteral("changed.mov"), QStringLiteral("Changed"), {}, {}, {}, {}, QString{}, QString{}, CompareRow::Changed, false});
-        rows.push_back(CompareRow{QStringLiteral("only-a.mov"), QStringLiteral("Only in A"), {}, {}, {}, {}, QString{}, QString{}, CompareRow::OnlyInA, false});
-        rows.push_back(CompareRow{QStringLiteral("only-b.mov"), QStringLiteral("Only in B"), {}, {}, {}, {}, QString{}, QString{}, CompareRow::OnlyInB, false});
-        rows.push_back(CompareRow{QStringLiteral("folder"), QStringLiteral("Folder only in A"), {}, {}, {}, {}, QString{}, QString{}, CompareRow::FolderOnlyInA, true});
+        rows.push_back(CompareRow{QStringLiteral("match.mov"),
+                                  QStringLiteral("Matching"),
+                                  {},
+                                  {},
+                                  {},
+                                  {},
+                                  QString{},
+                                  QString{},
+                                  CompareRow::Matching,
+                                  false});
+        rows.push_back(CompareRow{QStringLiteral("changed.mov"),
+                                  QStringLiteral("Changed"),
+                                  {},
+                                  {},
+                                  {},
+                                  {},
+                                  QString{},
+                                  QString{},
+                                  CompareRow::Changed,
+                                  false});
+        rows.push_back(CompareRow{QStringLiteral("only-a.mov"),
+                                  QStringLiteral("Only in A"),
+                                  {},
+                                  {},
+                                  {},
+                                  {},
+                                  QString{},
+                                  QString{},
+                                  CompareRow::OnlyInA,
+                                  false});
+        rows.push_back(CompareRow{QStringLiteral("only-b.mov"),
+                                  QStringLiteral("Only in B"),
+                                  {},
+                                  {},
+                                  {},
+                                  {},
+                                  QString{},
+                                  QString{},
+                                  CompareRow::OnlyInB,
+                                  false});
+        rows.push_back(CompareRow{QStringLiteral("folder"),
+                                  QStringLiteral("Folder only in A"),
+                                  {},
+                                  {},
+                                  {},
+                                  {},
+                                  QString{},
+                                  QString{},
+                                  CompareRow::FolderOnlyInA,
+                                  true});
         model.setRows(rows);
 
         CompareFilterProxyModel proxy;
@@ -43,30 +109,44 @@ private slots:
 
         proxy.setFilterMode(CompareFilterProxyModel::Matching);
         QCOMPARE(proxy.rowCount(), 1);
-        QCOMPARE(proxy.data(proxy.index(0, 1), Qt::DisplayRole).toString(), QStringLiteral("match.mov"));
+        QCOMPARE(proxy.data(proxy.index(0, 1), Qt::DisplayRole).toString(),
+                 QStringLiteral("match.mov"));
 
         proxy.setFilterMode(CompareFilterProxyModel::Changed);
         QCOMPARE(proxy.rowCount(), 1);
-        QCOMPARE(proxy.data(proxy.index(0, 1), Qt::DisplayRole).toString(), QStringLiteral("changed.mov"));
+        QCOMPARE(proxy.data(proxy.index(0, 1), Qt::DisplayRole).toString(),
+                 QStringLiteral("changed.mov"));
 
         proxy.setFilterMode(CompareFilterProxyModel::OnlyA);
         QCOMPARE(proxy.rowCount(), 1);
-        QCOMPARE(proxy.data(proxy.index(0, 1), Qt::DisplayRole).toString(), QStringLiteral("only-a.mov"));
+        QCOMPARE(proxy.data(proxy.index(0, 1), Qt::DisplayRole).toString(),
+                 QStringLiteral("only-a.mov"));
 
         proxy.setFilterMode(CompareFilterProxyModel::OnlyB);
         QCOMPARE(proxy.rowCount(), 1);
-        QCOMPARE(proxy.data(proxy.index(0, 1), Qt::DisplayRole).toString(), QStringLiteral("only-b.mov"));
+        QCOMPARE(proxy.data(proxy.index(0, 1), Qt::DisplayRole).toString(),
+                 QStringLiteral("only-b.mov"));
 
         proxy.setFilterMode(CompareFilterProxyModel::Folders);
         QCOMPARE(proxy.rowCount(), 1);
-        QCOMPARE(proxy.data(proxy.index(0, 1), Qt::DisplayRole).toString(), QStringLiteral("folder"));
+        QCOMPARE(proxy.data(proxy.index(0, 1), Qt::DisplayRole).toString(),
+                 QStringLiteral("folder"));
     }
 
     void emitsRowsChangedOnReset()
     {
         CompareResultTableModel model;
         QSignalSpy spy(&model, &CompareResultTableModel::rowsChanged);
-        model.setRows({CompareRow{QStringLiteral("clip.mov"), QStringLiteral("Matching"), {}, {}, {}, {}, QString{}, QString{}, CompareRow::Matching, false}});
+        model.setRows({CompareRow{QStringLiteral("clip.mov"),
+                                  QStringLiteral("Matching"),
+                                  {},
+                                  {},
+                                  {},
+                                  {},
+                                  QString{},
+                                  QString{},
+                                  CompareRow::Matching,
+                                  false}});
         QCOMPARE(spy.count(), 1);
     }
 };
